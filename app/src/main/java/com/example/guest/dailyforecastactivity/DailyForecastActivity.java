2dbd64c1ae56c1e740da4d86a551475b39e95b52
Adam <wonton916@gmail.com>
2015-12-02 11:57:50 -0800

    Daily Forecast android app, initial files using adapters and list view

---
+++ app/src/main/java/com/example/guest/dailyforecastactivity/DailyForecastActivity.java
@@ -0,0 +1,19 @@
+package com.example.guest.dailyforecastactivity;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
+    }
+}
